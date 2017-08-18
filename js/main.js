7416f5341ca644d942a088bc22ffe51b2fcc30cf
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-08-17 21:15:44 -0600

    Added some details :books:

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
   $(window).bind('scroll', function() {
-    // The value of where the "scoll" is.
+    // The value of where the "scroll" is.
     if($(window).scrollTop() > 175){
       $('nav').addClass('sticky');
     }else{
       $('nav').removeClass('sticky');
     }
   })
+  var counter = 0;
+  var images = ['img/banner.png','img/banner2.jpg','img/banner3.jpg','img/banner4.jpg','img/banner5.jpg'];
+
+  $(function() {
+    changeImages();
+    function changeImages() {
+      setTimeout(changeImages,4200);
+      if(counter == images.length-1) {
+        counter = 0;
+        $('#pautImage').attr("src",images[counter]);
+      } else {
+        counter++;
+        $('#pautImage').attr("src",images[counter]);
+      }
+    }
+  });
 });
